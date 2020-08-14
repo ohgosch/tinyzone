b1d99b84b5c33405513638007f576fba237bb9f3
Gosch <dev@gosch.io>
2020-08-13 23:35:14 -0300

    Change env

--- logic/requests/links.ts
+++ logic/requests/links.ts
@@ -1,5 +1,5 @@
 export const getLinks = async () => {
-  const baseAPI = process.env.REACT_APP_BASE_API;
+  const baseAPI = process.env.NEXT_BASE_API;
   const res = await fetch(`${baseAPI}/links`);
   const data = await res.json();
   return data;
